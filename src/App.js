56e42e3069f9348b0d399518fe74d54d6aeee79c
MartinProf <crosemont.martin@gmail.com>
2022-11-28 09:37:31 -0500

    ajout navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import ChoixInstallation from './controle/RegionAdministrative';
+import ChoixInstallation from './controle/ChoixInstallation';
+import Navbar from './controle/Navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar /><br/><br/><br/><br/>
       <ChoixInstallation />
     </div>
   );
