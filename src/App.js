041911386e13f4bd9fb54ff2ec88b27e2a31d850
MartinProf <crosemont.martin@gmail.com>
2022-12-09 09:35:39 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { useEffect, useState } from 'react';
+//import { useState } from 'react';
 import './App.css';
 import ChoixInstallation from './controle/ChoixInstallation';
+import DetailsInsta from './controle/DetailsInsta';
 import Navbar from './controle/Navbar';
 
 
 function App() {
-    const [items, setItems] = useState([])
 
-    useEffect( () => {
-       fetch('https://www.donneesquebec.ca/recherche/api/3/action/datastore_search?resource_id=a9272cc9-8234-40d1-9806-9f6b4c75c20d&limit=150')
-      .then(response => response.json())
-      .then(data => setItems(data.result.records)
-        );
-    }, [])
+  //const [ install, setInstall ] = useState();
 
 
   return (
     <div className="App">
       <Navbar /><br/><br/><br/><br/>
       <ChoixInstallation />
+      <DetailsInsta listeComplete="bonjour" />
       <h1>{}</h1>
-      {items.map(item => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
+
     </div>
   );
 }
---
+++ src/controle/DetailsInsta.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { Card , Icon } from "semantic-ui-react";
+
+function DetailsInsta ( props ) {
+    
+    const [ data, setData ] = useState([]);
+
+    useEffect( () => {
+       fetch('https://www.donneesquebec.ca/recherche/api/3/action/datastore_search?resource_id=a9272cc9-8234-40d1-9806-9f6b4c75c20d&limit=150')
+      .then(response => response.json())
+      .then(data => setData(data.result.records)
+        );
+    }, []);
+    
+    
+    /*
+        <div>
+            {data.map(item => {
+                return <pre>{JSON.stringify(item)}</pre>
+            })}
+        </div>
+    */
+
+    return(
+        <>
+        <Card>
+            <Card.Content>
+                <Card.Header>{data[0]._id}</Card.Header>
+            <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+                Matthew is a musician living in Nashville.
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+            <p>
+                <Icon name='user' />
+                22 Friends
+            </p>
+            </Card.Content>
+        </Card>
+        </>
+    );
+
+}
+
+export default DetailsInsta;
